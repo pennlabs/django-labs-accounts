d0735cdb71922e5de1a914b973451beac84b9dc1
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-03-24 19:51:24 -0400

    Add tests

---
+++ tests/test_apps.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from accounts.apps import AccountsConfig
+
+
+class AppsTestCase(TestCase):
+    def test_apps(self):
+        self.assertEqual(AccountsConfig.name, 'accounts')
