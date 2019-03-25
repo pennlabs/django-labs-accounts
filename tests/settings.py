9ef9f1b8b240fc056bcb81d3bc6e70a542a8c207
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-03-25 12:20:22 -0400

    Fix CircleCI test results

--- tests/settings.py
+++ tests/settings.py
@@ -77,3 +77,7 @@ PLATFORM_ACCOUNTS = {
     'CLIENT_SECRET': 'secret',
     'REDIRECT_URI': 'example',
 }
+
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_VERBOSE = 2
+TEST_OUTPUT_DIR = 'test-results'
