1249236180c80dc831db793de801187a1d769805
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-23 20:04:53 -0400

    Update tests

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -6,22 +6,55 @@ from django.test import TestCase
 class BackendTestCase(TestCase):
     def setUp(self):
         self.User = get_user_model()
+        self.remote_user = {
+            'major': 'Major',
+            'school': 'School',
+            'first_name': 'First',
+            'last_name': 'Last',
+            'username': 'user',
+            'email': 'test@test.com',
+            'affiliation': [],
+            'product_permission': []
+        }
 
     def test_invalid_remote_user(self):
         user = auth.authenticate(remote_user=None)
         self.assertIsNone(user)
 
     def test_create_user(self):
-        auth.authenticate(remote_user='00000000000000000000000000000001')
+        auth.authenticate(remote_user=self.remote_user)
         self.assertEqual(len(self.User.objects.all()), 1)
-        self.assertEqual(str(self.User.objects.all()[0].uuid), '00000000-0000-0000-0000-000000000001')
-        self.assertEqual(str(self.User.objects.all()[0]), '00000000000000000000000000000001')
+        user = self.User.objects.all()[0]
+        self.assertEqual(user.username, 'user')
+        self.assertEqual(user.first_name, 'First')
+        self.assertEqual(user.last_name, 'Last')
+        self.assertEqual(user.email, 'test@test.com')
+        self.assertFalse(self.User.objects.all()[0].is_staff)
 
     def test_login_user(self):
         student = self.User.objects.create_user(
-            username='00000000000000000000000000000001',
-            uuid='00000000000000000000000000000001',
+            username='user',
             password='secret'
         )
-        user = auth.authenticate(remote_user='00000000000000000000000000000001')
+        user = auth.authenticate(remote_user=self.remote_user)
         self.assertEqual(user, student)
+        self.assertEqual(len(self.User.objects.all()), 1)
+        self.assertFalse(self.User.objects.all()[0].is_staff)
+
+    def test_login_user_admin(self):
+        self.remote_user['product_permission'] = ['example_admin']
+        student = self.User.objects.create_user(
+            username='user',
+            password='secret'
+        )
+        user = auth.authenticate(remote_user=self.remote_user)
+        self.assertEqual(user, student)
+        self.assertEqual(len(self.User.objects.all()), 1)
+        self.assertTrue(self.User.objects.all()[0].is_staff)
+
+    def test_create_user_admin(self):
+        self.remote_user['product_permission'] = ['example_admin']
+        auth.authenticate(remote_user=self.remote_user)
+        self.assertEqual(len(self.User.objects.all()), 1)
+        self.assertEqual(self.User.objects.all()[0].username, 'user')
+        self.assertTrue(self.User.objects.all()[0].is_staff)
