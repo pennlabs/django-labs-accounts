d6cc4d2e4de5cd438b28d0ebd789f7938b3035c2
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-23 20:04:43 -0400

    Modify authentication to fit platform

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,15 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import RemoteUserBackend
 
+from accounts.settings import accounts_settings
+
 
 class LabsUserBackend(RemoteUserBackend):
     def authenticate(self, request, remote_user):
         if not remote_user:
             return
         User = get_user_model()
-        user, created = User.objects.get_or_create(uuid=remote_user)
+        user, created = User.objects.get_or_create(username=remote_user['username'])
         if created:
-            user.username = user.uuid
+            user.first_name = remote_user['first_name']
+            user.last_name = remote_user['last_name']
+            user.email = remote_user['email']
             user.set_unusable_password()
             user.save()
             try:
@@ -17,4 +21,8 @@ class LabsUserBackend(RemoteUserBackend):
             except TypeError:
                 user = self.configure_user(user)
 
+        if accounts_settings.ADMIN_PERMISSION in remote_user['product_permission']:
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
         return user if self.user_can_authenticate(user) else None
