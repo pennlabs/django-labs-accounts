5ae0a9b3bce1c390d391cd00ec08dc4174a0f843
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-04-26 21:22:10 -0400

    Set up CircleCI publishing to PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
+import os
+import sys
+
 from setuptools import setup
+from setuptools.command.install import install
+
+VERSION = "0.3.2"
 
 
-setup()
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
+setup(
+    version=VERSION,
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
+)
