3191c1a76f17cf58ead9056c59328d3f474cf816
ArmaanT <13340433+ArmaanT@users.noreply.github.com>
2019-12-02 17:29:20 -0500

    Adjust package finding

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from setuptools import setup
+from setuptools import find_packages, setup
 from setuptools.command.install import install
 
 
@@ -26,7 +26,7 @@ class VerifyVersionCommand(install):
 setup(
     name='django-labs-accounts',
     version=VERSION,
-    packages=['accounts'],
+    packages=find_packages(exclude=['tests']),
     url='https://github.com/pennlabs/django-labs-accounts',
     project_urls={
         'Changelog': ('https://github.com/pennlabs/django-labs-accounts/blob/master/CHANGELOG.md')
