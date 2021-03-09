add5d40e1b63bd4a377f995040f56b80d8e0109a
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-03-09 17:45:48 -0500

    Refactor attest + jwks functions

--- identity/identity.py
+++ identity/identity.py
@@ -1,35 +1,48 @@
-from django.conf import settings
 from jwcrypto import jwt, jwk
-from urllib.parse import urljoin
 import requests
 import time
 import json
 
+from accounts.settings import accounts_settings
 
-class IdentityContainer():
+
+JWKS_URL = f"{accounts_settings.PLATFORM_URL}/identity/jwks/"
+ATTEST_URL = f"{accounts_settings.PLATFORM_URL}/identity/attest/"
+REFRESH_URL = f"{accounts_settings.PLATFORM_URL}/identity/refresh/"
+
+
+class IdentityContainer:
     refresh_jwt: jwt.JWT = None
     access_jwt: jwt.JWT = None
     platform_jwks: jwk.JWKSet = None
 
 
-JWKS_URL = urljoin(settings["PLATFORM_URL"], "identity/jwks/")
-ATTEST_URL = urljoin(settings["PLATFORM_URL"], "identity/attest/")
-REFRESH_URL = urljoin(settings["PLATFORM_URL"], "identity/refresh/")
-
-# need to declare container so that we can modify access, refresh, and platform jw(t|ks)
 container = IdentityContainer()
 
 
 def get_platform_jwks():
-    response = requests.get(JWKS_URL)
-    content = response.json()
-    container.platform_jwks = jwk.JWKSet.from_json(content)
+    """
+    Download the JWKS from Platform to verify JWTS
+    """
+
+    try:
+        response = requests.get(JWKS_URL)
+        content = response.json()
+        # For some reason this method wants a raw string instead of a python dictionary
+        container.platform_jwks = jwk.JWKSet.from_json(response.text)
+    except Exception:
+        container.platform_jwks = None
 
 
 def attest():
-    client_id = settings["CLIENT_ID"]
-    client_secret = settings["CLIENT_SECRET"]
-    response = requests.post(ATTEST_URL, auth=(client_id, client_secret))
+    """
+    Perform the initial authentication (attest) with Platform using the Client ID
+    and Secret from DOT
+    """
+
+    response = requests.post(
+        ATTEST_URL, auth=(accounts_settings.CLIENT_ID, accounts_settings.CLIENT_SECRET)
+    )
     content = response.json()
     container.access_jwt = jwt.JWT(key=container.platform_jwks, jwt=content["access"])
     container.refresh_jwt = jwt.JWT(key=container.platform_jwks, jwt=content["refresh"])
@@ -41,9 +54,7 @@ def refresh_if_outdated():
     if time.time() < access_claims["exp"]:
         return
 
-    auth_headers = {
-        "Authorization": f"Bearer {container.refresh_jwt.serialize()}",
-    }
+    auth_headers = {"Authorization": f"Bearer {container.refresh_jwt.serialize()}"}
     response = requests.get(REFRESH_URL, headers=auth_headers)
     content = response.json()
     container.access_jwt = jwt.JWT(key=container.platform_jwks, jwt=content["access"])
