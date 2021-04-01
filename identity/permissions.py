961777a7f18e8fa0542f83da78050e447a6e21ff
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-04-01 10:24:08 -0400

    Make B2BPermission parameterizable

--- identity/permissions.py
+++ identity/permissions.py
@@ -6,24 +6,40 @@ from rest_framework import permissions
 from identity.identity import container
 
 
-class B2BPermission(permissions.BasePermission):
+def B2BPermission(urn):
     """
-    Grants permission if the current user is a superuser.
-    If authentication is successful `request.product` is set
-    to the urn of the product making the request.
+    Create a B2BPermission that only grants access to a product with
+    the provided urn.
+    Alternatively set urn to `all` to grant access to all Penn Labs products
     """
 
-    def has_permission(self, request, view):
-        authorization = request.META.get("HTTP_AUTHORIZATION")
-        if authorization and " " in authorization:
-            auth_type, raw_jwt = authorization.split()
-            if auth_type == "Bearer":
-                try:
-                    validated_jwt = jwt.JWT(key=container.platform_jwks, jwt=raw_jwt)
-                    claims = json.loads(validated_jwt.claims)
-                    if "use" in claims and claims["use"] == "access":
-                        request.product = claims["sub"]
-                        return True
-                except Exception:
-                    return False
-        return False
+    class B2BPermissionInner(permissions.BasePermission):
+        """
+        Grants permission if the current user is a superuser.
+        If authentication is successful `request.product` is set
+        to the urn of the product making the request.
+        """
+
+        def has_permission(self, request, view):
+            self.urn = urn
+            # Get Authorization header
+            authorization = request.META.get("HTTP_AUTHORIZATION")
+            if authorization and " " in authorization:
+                auth_type, raw_jwt = authorization.split()
+                if auth_type == "Bearer":
+                    try:
+                        # Validate JWT
+                        validated_jwt = jwt.JWT(key=container.platform_jwks, jwt=raw_jwt)
+                        claims = json.loads(validated_jwt.claims)
+                        # Ensure JWT is an access JWT
+                        if "use" in claims and claims["use"] == "access":
+                            # Validate urn
+                            if self.urn == "all" or claims["sub"] == self.urn:
+                                # Expose product urn to view
+                                request.product = claims["sub"]
+                                return True
+                    except Exception:
+                        return False
+            return False
+
+    return B2BPermissionInner
--- tests/identity/test_permissions.py
+++ tests/identity/test_permissions.py
@@ -7,20 +7,32 @@ from identity.permissions import B2BPermission
 from tests.identity.utils import configure_container
 
 
-class AppsTestCase(TestCase):
+class B2BTPermissionestCase(TestCase):
     def setUp(self):
         configure_container(self)
-        self.permission = B2BPermission()
+        self.permission = B2BPermission("all")()
 
     def test_invalid_access_jwt(self):
         headers = {"HTTP_AUTHORIZATION": "Bearer abc"}
         request = MagicMock(META=headers)
         self.assertFalse(self.permission.has_permission(request, None))
 
-    def test_valid_access_jwt(self):
+    def test_valid_access_jwt_all_urn(self):
         headers = {"HTTP_AUTHORIZATION": f"Bearer {container.access_jwt.serialize()}"}
         request = MagicMock(META=headers)
         self.assertTrue(self.permission.has_permission(request, None))
+
+    def test_valid_access_jwt_right_urn(self):
+        self.permission = B2BPermission("org:pennlabs:example")()
+        headers = {"HTTP_AUTHORIZATION": f"Bearer {container.access_jwt.serialize()}"}
+        request = MagicMock(META=headers)
+        self.assertTrue(self.permission.has_permission(request, None))
+
+    def test_valid_access_jwt_wrong_urn(self):
+        self.permission = B2BPermission("fake:urn")()
+        headers = {"HTTP_AUTHORIZATION": f"Bearer {container.access_jwt.serialize()}"}
+        request = MagicMock(META=headers)
+        self.assertFalse(self.permission.has_permission(request, None))
 
     def test_refresh_jwt(self):
         headers = {"HTTP_AUTHORIZATION": f"Bearer {container.refresh_jwt.serialize()}"}
