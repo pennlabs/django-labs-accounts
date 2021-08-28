def4e4dd1a7583edc5d8ae7d06db390fec5178f5
rfax <rmarques@seas.upenn.edu>
2021-08-28 12:22:30 -0300

    Added custom authentication

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -2,6 +2,8 @@ import requests
 from django.contrib import auth
 from django.contrib.auth import get_user_model
 from django.http import HttpResponseForbidden, HttpResponseServerError
+from rest_framework import authentication
+from rest_framework import exceptions
 
 from accounts.settings import accounts_settings
 
@@ -55,3 +57,48 @@ class OAuth2TokenMiddleware:
 
         response = self.get_response(request)
         return response
+
+
+class ProductAuthentication(authentication.BaseAuthentication):
+    """
+    When a view is requested using a Bearer Authorization header,
+    check and set request.user to the owner of said token
+    """
+
+    def authenticate(self, request):
+        authorization = request.META.get("HTTP_AUTHORIZATION")
+        if authorization and " " in authorization:
+            auth_type, token = authorization.split()
+            if auth_type == "Bearer":  # Only validate if Authorization header type is Bearer
+                body = {"token": token}
+                headers = {"Authorization": "Bearer {}".format(token)}
+                try:
+                    platform_request = requests.post(
+                        url=accounts_settings.PLATFORM_URL + "/accounts/introspect/",
+                        headers=headers,
+                        data=body,
+                    )
+                    if platform_request.status_code == 200:  # Access token is valid
+                        json = platform_request.json()
+                        user_props = json["user"]
+                        # TODO: maybe update access token if a local one does not exist
+                        # user_props["token"] = {
+                        #     "access_token": token,
+                        #     "refresh_token": "",
+                        #     "expires_in": json["exp"] - int(timezone.now().timestamp()),
+                        # }
+
+                        user = auth.authenticate(remote_user=user_props, tokens=False)
+                        if user:  # User authenticated successfully
+                            return (user, None)
+                        else:  # Error occurred
+                            raise exceptions.AuthenticationFailed('No such user')
+                    else:  # Access token is invalid
+                        raise exceptions.AuthenticationFailed('Invalid Token')
+                except requests.exceptions.RequestException:  # Can't connect to platform
+                    # Throw a 403 because we can't verify the incoming access token so we
+                    # treat it as invalid. Ideally platform will never go down, so this
+                    # should never happen.
+                    return HttpResponseForbidden()
+
+        return None
