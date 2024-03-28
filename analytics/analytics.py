67d95ca4beaa908da2ed0c188f25c0bb14e0b656
Justin Zhang <judtin@seas.upenn.edu>
2024-03-27 22:04:59 -0400

    AnalyticsTxn

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -1,6 +1,44 @@
+import json
 from concurrent.futures import ThreadPoolExecutor
+from enum import Enum
+from typing import Optional
 
 import requests
+from django.utils import timezone
+
+
+class Product(Enum):
+    OTHER = 0
+    MOBILE_IOS = 1
+    MOBILE_ANDROID = 2
+    MOBILE_BACKEND = 3
+    PORTAL = 4
+    PCR = 5
+    PDP = 6
+    PCA = 7
+    PCP = 8
+    OHQ = 9
+    CLUBS = 10
+
+    def __str__(self):
+        return self.name
+
+
+class AnalyticsTxn:
+    def __init__(
+        self,
+        product: Product,
+        pennkey: Optional[str],
+        timestamp=timezone.now(),
+        data=dict(),
+    ):
+        self.product = str(product)
+        self.pennkey = pennkey
+        self.timestamp = timestamp.timestamp()
+        self.data = data
+
+    def to_json(self):
+        return json.dumps(vars(self))
 
 
 class LabsAnalytics:
@@ -19,9 +57,9 @@ class LabsAnalytics:
     def __init__(self):
         self.executor = ThreadPoolExecutor(max_workers=self.POOL_SIZE)
 
-    def submit(self, json):
+    def submit(self, txn: AnalyticsTxn):
         headers = {}
-        self.executor.submit(self.request_job, json, headers)
+        self.executor.submit(self.request_job, txn.to_json(), headers)
 
     def request_job(self, json, headers):
         requests.post(self.ANALYTICS_URL, json=json, headers=headers)
